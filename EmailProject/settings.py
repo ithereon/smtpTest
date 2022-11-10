7bfe9a0b6dca59ab216519968c3d390ee44acab4
smith <jamesmith110111@gmail.com>
2022-11-10 00:21:32 -0800

    safds

--- EmailProject/settings.py
+++ EmailProject/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contact',
-    'crispy_forms',
+    # 'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
